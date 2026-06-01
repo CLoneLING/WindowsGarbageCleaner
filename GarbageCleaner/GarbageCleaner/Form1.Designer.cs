5877ca0b6c15290a79b9d4e2d5580b5fab07badb
YuMoo <128892215+CLoneLING@users.noreply.github.com>
2026-06-02 07:06:13 +0800

    Add files via upload

---
+++ GarbageCleaner/GarbageCleaner/Form1.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace GarbageCleaner
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageMain;
+        private System.Windows.Forms.TabPage tabPageSettings;
+        private System.Windows.Forms.TabPage tabPageAbout;
+        private System.Windows.Forms.TabPage tabPageDonate;
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.CheckBox chkWindowsUpdate;
+        private System.Windows.Forms.CheckBox chkEdgeCache;
+        private System.Windows.Forms.CheckBox chkEventLogs;
+        private System.Windows.Forms.CheckBox chkRecentDocs;
+        private System.Windows.Forms.CheckBox chkPrefetch;
+        private System.Windows.Forms.CheckBox chkRecycleBin;
+        private System.Windows.Forms.CheckBox chkTempFiles;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnDeselectAll;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label lblAppName;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.Label lblUpdateDate;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.LinkLabel linkEmail;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblNotice;
+
+        // 主控件
+
+    }
+}
